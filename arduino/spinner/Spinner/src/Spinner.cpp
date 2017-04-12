a643a25aba5aca25499b878d9fe617481cf58617
James Stephens <james.h.stephens@protonmail.com>
2017-04-12 18:25:26 -0400

    Adding Unity Avatar code

---
+++ arduino/spinner/Spinner/src/Spinner.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#include "tserial.h"
+#include "bot_control.h"
+
+serial comm; //serial is a class type defined in these files, used for referring to the communication device
+
+int main()
+{
+	char data; //To store the character to send
+	printf("Enter character to be sent"); //User prompt
+	scanf_s("%c", &data); //User input
+	comm.startDevice("COM3", 9600);
+	/* “COM 2” refers to the com port in which the USB to SERIAL port is attached. It is shown by right clicking on my computer, then going to properties and then device manager
+	9600 is the baud-rate in bits per second */
+	comm.send_data(data); //The data is sent through the port
+	comm.stopDevice(); //The device is closed down
+	_getch();
+}
+
---
+++ arduino/spinner/Spinner/src/serial.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+#include "tserial.h"
+#include "bot_control.h"
+
+serial comm; //serial is a class type defined in these files, used for referring to the communication device
+
+void main() {
+
+}
