5ec4275b4cffa8ddf4bb29276535a3297c846119
Greg Melencio <greg@abantech.net>
2017-05-21 20:03:58 -0400

    Updated to use Bone on Avatar

--- unity/Avatar/IViewPortLocationProvider.cs
+++ unity/Avatar/IViewPortLocationProvider.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 
 /// <summary>
 /// Implement to defetermine the viewport position & orientation. This may be attached to the MainCamera or to whatever causes the head position and orientation to change.
-/// </summary>
+/// </summary>   
 interface IViewPortLocationProvider
 {
-    Vector3 CurrentWorldPosition { get; set; }
+
+    Vector3 CurrentWorldPosition { get; set; } 
     Quaternion CurrentOrientation { get; set; }  
-}
+} 
