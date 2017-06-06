f1fe103a22fa3064cc625bd711d27909402c9af7
Greg Melencio <greg@abantech.net>
2017-06-05 22:21:16 -0400

    Joints mapped to Vivie

--- unity/Avatar/Assets/StickManGenerator.cs
+++ unity/Avatar/Assets/StickManGenerator.cs
@@ -19,7 +19,7 @@ public class StickManGenerator : AvatarGenerator
     //Use this to find the stabilized position of joints
     private Dictionary<HumanJointType, List<Vector3>> JointPositionsLog;
     //Defines how many entries are stored for a given joint before it is removed
-    public int JointPositionsMaxLogCount = 60; 
+    public int JointPositionsMaxLogCount = 60;
 
     public float hmdHeadPositionX;
     public float hmdHeadPositionY;
@@ -29,6 +29,9 @@ public class StickManGenerator : AvatarGenerator
     public float figHeadPositionY;
     public float figHeadPositionZ;
 
+    public float kinectCoordinateRotationCorrection;
+    //Needed to convert counterclockwise rotation into clockwise
+    private float invertedDegreeKinectCoordinateRotationCorrection;
 
 
     //Some Motion Captures do mirror image, adjust these to do the inverse to compensate as required
@@ -47,8 +50,12 @@ public class StickManGenerator : AvatarGenerator
         figHeadPositionZ = 0;
         //OnJointGeneratedActions = new Dictionary<HumanJointType, Action<GameObject>>();
         //OnJointUpdateActions = new Dictionary<HumanJointType, Action<GameObject>>();
-        if (invertMocapX)
-            this.gameObject.transform.parent.position = Vector3.Scale(new Vector3(-1, 1, 1), this.gameObject.transform.parent.position);
+
+        // Convert counter clockwise to clockwise
+        invertedDegreeKinectCoordinateRotationCorrection = -kinectCoordinateRotationCorrection;
+
+        // Rotate all coordinates around the world down vector by the kinect rotation vector to offset kinect rotation
+        this.transform.Rotate(Vector3.down, invertedDegreeKinectCoordinateRotationCorrection, Space.World);
     }
 
     private BodyJointPositionMapping MoCapDataSource
@@ -68,12 +75,20 @@ public class StickManGenerator : AvatarGenerator
         if (figureGenerated)
         {
             var headPosition = jointGameObjects[HumanJointType.Head].transform.position;
-            MoCapDataSource.Offset = MoCapDataSource.Offset + new Vector3(-MoCapDataSource.HeadPosition.x, -MoCapDataSource.HeadPosition.y, -MoCapDataSource.HeadPosition.z) + HeadAnchorObject.transform.position;
 
+            // Convert degrees to rads
+            var rads = Math.PI * invertedDegreeKinectCoordinateRotationCorrection / 180.0;
 
-            Vector3 mocapCoordinateInversion = new Vector3(invertMocapX ? -1f : 1f, invertMocapY ? -1f : 1f, invertMocapZ ? -1 : 1);
+            // Create x coordinate as a function of the rotation of the kinect
+            var cameraForKinectx = Math.Cos(rads) * HeadAnchorObject.transform.position.x + Math.Sin(rads) * HeadAnchorObject.transform.position.z;
 
+            var cameraForKinecty = HeadAnchorObject.transform.position.y;
 
+            // Create z coordinate as a function of the rotation of the kinect
+            var cameraForKinectz = Math.Sin(rads) * HeadAnchorObject.transform.position.x - Math.Cos(rads) * HeadAnchorObject.transform.position.z;
+
+            // Create offset
+            MoCapDataSource.Offset = MoCapDataSource.Offset + Vector3.Scale(new Vector3(-1, -1, -1), MoCapDataSource.HeadPosition) + new Vector3((float)cameraForKinectx, cameraForKinecty, (float)cameraForKinectz);
 
             //Update the joint positions
             foreach (var jointType in BodyJointPositionMapping.GetAllJointTypes())
@@ -82,25 +97,27 @@ public class StickManGenerator : AvatarGenerator
                 //var jointType = HumanJointTypeUtil.OppositeSideJoint(humanjointType);
                 if (jointGameObjects.ContainsKey(jointType))
                 {
-                    if(MoCapDataSource.TryGetMappedJointPosition(jointType, out newJointPosition))
+                    if (MoCapDataSource.TryGetMappedJointPosition(jointType, out newJointPosition))
                     {
                         Vector3 adjustedPosition = newJointPosition;
 
                         //This approach flips the axes indiscrimnately
-                        //adjustedPosition = Vector3.Scale(new Vector3(invertMocapX ? -1f : 1f, invertMocapY ? -1f : 1f, invertMocapZ ? -1 : 1), newJointPosition);
+                        adjustedPosition = Vector3.Scale(new Vector3(invertMocapX ? -1f : 1f, invertMocapY ? -1f : 1f, invertMocapZ ? -1 : 1), newJointPosition);
 
                         //This apporach only flips the left and right sides of the body, otherwise even the movement is affected (i.e. toward/away)
                         //adjustedPosition = invertMocapX && (jointType.ToString().Contains("Left") || jointType.ToString().Contains("Right")) ? Vector3.Scale(newJointPosition, new Vector3(-1, 1, 1)) : newJointPosition;
                         //adjustedPosition = invertMocapY ? Vector3.Scale(adjustedPosition, new Vector3(1, -1, 1)) : adjustedPosition;
                         //adjustedPosition = invertMocapZ ? Vector3.Scale(adjustedPosition, new Vector3(1, 1, -1)) : adjustedPosition;
-                        jointGameObjects[jointType].transform.position = adjustedPosition;
+
+                        // Use local position so container rotation is applied to all child joints
+                        jointGameObjects[jointType].transform.localPosition = adjustedPosition;
                     }
                     else
                     {
                         //TODO: interpolate position here?
                         Debug.LogWarningFormat("No position mapped to joint '{0}'", jointType.ToString());
                     }
-                    
+
                 }
 
                 //if (OnJointGeneratedActions.ContainsKey(jointType))
@@ -135,11 +152,11 @@ public class StickManGenerator : AvatarGenerator
         {
             JointPositionsLog[joint] = new List<Vector3>() { currentPosition };
         }
-       else
+        else
         {
             JointPositionsLog[joint].Add(currentPosition);
         }
-        
+
         //Remove the oldest logged position if we've exceeded the maximum log count
         if (JointPositionsLog[joint].Count > JointPositionsMaxLogCount)
         {
@@ -164,6 +181,9 @@ public class StickManGenerator : AvatarGenerator
 
     private void CreateJoints()
     {
+        // Transparent joints so looking down doesn't cause blindness
+        HumanJointType[] transparentJoints = { HumanJointType.Head, HumanJointType.Neck, HumanJointType.SpineShoulder};
+
         Debug.LogWarningFormat("Joint Game Objects being Created!!");
         foreach (var jointType in BodyJointPositionMapping.GetAllJointTypes())
         {
@@ -171,14 +191,24 @@ public class StickManGenerator : AvatarGenerator
             try
             {
                 var jointGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                jointGameObject.transform.localScale = new Vector3(jointSize, jointSize, jointSize);
+
+                if (transparentJoints.Contains(jointType))
+                {
+                    // Make transparent joints infinitely small
+                    jointGameObject.transform.localScale = new Vector3(0, 0, 0);
+                }
+                else
+                {
+                    jointGameObject.transform.localScale = new Vector3(jointSize, jointSize, jointSize);
+                }
+
                 jointGameObject.name = jointType.ToString();
                 currentJointName = jointType.ToString();
                 jointGameObject.GetComponent<Renderer>().material = jointMaterial;
                 jointGameObject.transform.parent = this.transform;
                 jointGameObjects.Add(jointType, jointGameObject);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Debug.LogWarningFormat("Create joint '{0}' Failed. Error: {1}, Message: {2}", currentJointName, ex.GetType().Name, ex.Message);
             }
@@ -190,7 +220,8 @@ public class StickManGenerator : AvatarGenerator
     {
         bones = new List<Bone>();
         // Head - Shoulder
-        bones.Add(Bone.Create(jointGameObjects[HumanJointType.Head], jointGameObjects[HumanJointType.SpineShoulder]));
+        // Remove neck bone
+        //bones.Add(Bone.Create(jointGameObjects[HumanJointType.Head], jointGameObjects[HumanJointType.SpineShoulder]));
         // Sholder - Shoulder Right
         bones.Add(Bone.Create(jointGameObjects[HumanJointType.SpineShoulder], jointGameObjects[HumanJointType.ShoulderRight]));
         // Shoulder Right - Elbow
