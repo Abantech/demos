d556ece6b25f0444269f621c56af62a5e0deeb3a
Greg Melencio <greg@abantech.net>
2017-06-05 22:14:26 -0400

    Added device manager to know what devices are available at any time

---
+++ unity/Avatar/Assets/DeviceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    class DeviceManager
+    {
+        public static bool LeapEnabled = false;
+        public static bool KinectEnabled = false;
+        public static bool ViveEnabled = true;
+    }
+}
